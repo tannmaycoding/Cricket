5175de505e282f98cf28a12886c3ce9226a0979f
Tannmay Khandelwal <94522751+tannmaycoding@users.noreply.github.com>
2025-03-16 23:28:14 +0530

    Update combined.py

--- cricket/combined.py
+++ cricket/combined.py
@@ -1,10 +1,11 @@
-from batting.overall_shot_detection import CricketShotClassifier
-from balling.overall_bowl_detection import CricketBallAnalyzer
-import utils
+from .batting.overall_shot_detection import CricketShotClassifier
+from .balling.overall_bowl_detection import CricketBallAnalyzer
+import cricket.utils
 
 
 def classify_overall(video, ball_colour, pitch_length):
     bat = CricketShotClassifier()
     bat_decision = bat.classify_entire_video(video)
-    ball_decision = CricketBallAnalyzer(video, utils.color_ranges[ball_colour][0], utils.color_ranges[1], pitch_length)
+    ball = CricketBallAnalyzer(video, cricket.utils.color_ranges[ball_colour][0], cricket.utils.color_ranges[ball_colour][1], pitch_length)
+    ball_decision = ball.analyze_video()
     return {"ball": ball_decision, "bat": bat_decision}
